aadc4d1973a4198f08e07c793f5f3eada6e1e410
almulalic <almir.mulalic.am@gmail.com>
2020-03-26 18:16:34 +0100

    Dodan konsturktor getter i setter the dummy metoda za izbaci artikal u ArtikalTest folder

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rs.tut4;
+import java.util.ArrayList;
 
 public class Artikal {
+    private String sifra,naziv;
+    private double cijena;
+
+    public Artikal(String sifra, String naziv, double cijena) {
+        this.sifra = sifra;
+        this.naziv = naziv;
+        this.cijena = cijena;
+    }
+
+    public static void izbaciDuplikate(ArrayList<Artikal> lista) {
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
 }
