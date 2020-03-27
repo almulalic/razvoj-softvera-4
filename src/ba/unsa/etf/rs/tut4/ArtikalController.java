c39f7a9d88f744d760fc26a6d8f09be886046fa6
almulalic <almir.mulalic.am@gmail.com>
2020-03-27 12:51:23 +0100

    zavrsena implementacija UI interfejsa,UI povezan sa kontrolerom i zavrsen zadatak 2

---
+++ src/ba/unsa/etf/rs/tut4/ArtikalController.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.rs.tut4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.event.ActionEvent;
+
+import java.util.ArrayList;
+
+public class ArtikalController {
+    public TextArea unosArtikala;
+    public TextArea filtiraniArtikli;
+
+    public void DodajArtikal (ActionEvent actionEvent) {
+        ArrayList<Artikal> lista = new ArrayList<Artikal>();
+
+        String[] unos = unosArtikala.getText().split("\n");
+        String[] temp;
+
+        for(String red:unos){
+            temp = red.split(",");
+            lista.add(new Artikal(temp[0].trim(),temp[1].trim(),Double.parseDouble(temp[2].trim())));
+        }
+
+        Artikal.izbaciDuplikate(lista);
+
+        StringBuilder sb = new StringBuilder();
+        for(Artikal a:lista){
+            sb.append(a.sifra + ' ' + a.naziv + ' ' + a.cijena);
+            sb.append("\n");
+        }
+
+        filtiraniArtikli.setText(sb.toString());
+    }
+}
--- src/ba/unsa/etf/rs/tut4/Main.java
+++ src/ba/unsa/etf/rs/tut4/Main.java
@@ -1,7 +1,24 @@
 package ba.unsa.etf.rs.tut4;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Sretan rad na tutorijalu 4!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/UI.fxml"));
+        primaryStage.setTitle("Tutorijal 3");
+        primaryStage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        primaryStage.setMinHeight(120);
+        primaryStage.setMinWidth(200);
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {launch(args);}
 }
